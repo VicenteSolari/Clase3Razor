a863aa05be79d94fe1b4b1b3bff9eb3ee418c200
= <prokeso_yop@hotmai.com>
2021-12-22 18:03:45 -0300

    Ejercicio Razor

---
+++ Team.cs
@@ -0,0 +1,12 @@
+public class Team{
+
+    public Team(int id, string name)
+    {
+        Name = name;
+        Id = id;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+}
